e05073a89aea1fd14d0ab96064e83ef633de02aa
Srinivas <vasus4990@gmail.com>
2024-11-27 17:20:51 +0530

    About section is done

--- Animated-Gsap-site/src/App.jsx
+++ Animated-Gsap-site/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 export default function App() {
   return (
     <div className="relate mid-h-screen w-screen overflow-x-hidden">
       <Hero />
+      <About />
     </div>
   );
 }
