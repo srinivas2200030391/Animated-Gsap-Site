b59ebec9ec00b66257df9a87d92e3a0ed73b7f76
Srinivas <vasus4990@gmail.com>
2024-12-05 17:12:50 +0530

    small changes made

--- Animated-Gsap-site/src/components/Contact.jsx
+++ Animated-Gsap-site/src/components/Contact.jsx
@@ -35,11 +35,11 @@ const Contact = () => {
 
         <div className="flex flex-col items-center text-center">
           <p className="mb-10 font-general text-[10px] uppercase">
-            Join Zentry
+            Join Imaginarium Theatre
           </p>
 
           <AnimatedTitle
-            title="let&#39;s b<b>u</b>ild the <br /> new era of <br /> g<b>a</b>ming t<b>o</b>gether."
+            title="let&#39;s b<b>u</b>ild the <br /> new era of <br /><b>A</b>RTSPA<b>C</b><b>E</b>."
             className="special-font !md:text-[6.2rem] w-full font-zentry !text-5xl !font-black !leading-[.9]"
           />
 
