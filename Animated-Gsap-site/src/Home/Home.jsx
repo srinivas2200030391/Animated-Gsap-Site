27eafe886e8a82074feeac827372bed74e04c44f
Srinivas <vasus4990@gmail.com>
2024-12-05 16:36:26 +0530

    small changes

---
+++ Animated-Gsap-site/src/Home/Home.jsx
@@ -0,0 +1,20 @@
+import NavBar from "../components/NavBar";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Features from "../components/Features";
+import Story from "../components/Story";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
