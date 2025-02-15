4ac67ac69f090d024bf50ad54b4fa3a24cebe20b
Srinivas Kommirisetty <116977437+srinivas2200030391@users.noreply.github.com>
2025-02-15 16:21:50 +0530

    Update main.jsx

--- Animated-Gsap-site/src/main.jsx
+++ Animated-Gsap-site/src/main.jsx
@@ -5,9 +5,11 @@ import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
+  <div className="w-full overflow-x-hidden">
   <BrowserRouter>
     <StrictMode>
       <App />
     </StrictMode>
   </BrowserRouter>
+    </div>
 );
